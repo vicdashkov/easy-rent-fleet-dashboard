48250dafa5810d89b59f2df0f7a276966cda966a
vdashkov <vic.dashkov@gmail.com>
2019-07-06 21:15:07 -0700

    added static js. added order list template

---
+++ static/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {format: "d-m-yyyy"});
+    var selects = document.querySelectorAll('select');
+    var selectsInstances = M.FormSelect.init(selects, {});
+});
